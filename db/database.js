7bdadedf2d40ffcccabbce42c7899bade3d68600
Erin Von Gertzen <ianvon@shaw.ca>
2024-03-06 22:43:54 -0700

    refactored reservations, properties and add property

--- db/database.js
+++ db/database.js
@@ -115,20 +115,26 @@ const addUser =  function(user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  const promise = pool
-  .query(
-    "SELECT * FROM reservations WHERE guest_id = $1 LIMIT $2", [ guest_id, limit])
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  const values = [guest_id, limit];
+  return db.query(queryString, values)
     .then(res => {
-      return res.rows
+      return res.rows;
     })
     .catch(err => {
-      console.log(err)
-    })
-
-
-  
-  return promise
-}
+      return console.log('query error:', err);
+    });
+};
 
 /// Properties
 
